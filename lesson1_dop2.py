ba7221f1368f5220a7f9f3f13f7ead1dfe5a65b1
daryanovi <darya.novikova@gmail.com>
2018-03-04 15:16:24 +0300

    Добавила заголовки заданий

--- lesson1_dop2.py
+++ lesson1_dop2.py
@@ -1,5 +1,6 @@
 from collections import Counter
-# Задание 1
+task="Задание 1"
+print(task)
 # Дан список учеников, нужно посчитать количество повторений каждого имени ученика.
 students = [
 	{'first_name': 'Вася'},
@@ -20,10 +21,11 @@ for x in students:
 	sort_list.append(sort_var)
 count=Counter(sort_list)
 for y in count:
-	b="# {}: {}".format(y, count[y])
-	print(b)
+	text="# {}: {}".format(y, count[y])
+	print(text)
 
-# Задание 2
+task="Задание 2"
+print(task)
 # Дан список учеников, нужно вывести самое часто повторящееся имя.
 
 students = [
@@ -33,12 +35,19 @@ students = [
 	{'first_name': 'Маша'},
 	{'first_name': 'Оля'},
 ]
-# ???
 
 # Пример вывода:
 # Самое частое имя среди учеников: Маша
 
-# Задание 3
+sort_list=[]
+for x in students:
+	sort_var=x.get('first_name')
+	sort_list.append(sort_var)
+count=Counter(sort_list).most_common(1)[0]
+print("Самое распространенное имя среди учеников: {}".format(count[0]))
+
+task="Задание 3"
+print(task)
 # Есть список учеников в нескольких классах, нужно вывести самое частое имя в каждом классе.
 school_students = [
 	[  # это – первый класс
@@ -51,15 +60,26 @@ school_students = [
 		{'first_name': 'Оля'},
 	]
 ]
-# ???
 
 # Пример вывода:
 # Самое частое имя в классе 1: Вася
 # Самое частое имя в классе 2: Маша
 
 
-# Задание 4
+y=0
+for x in school_students:
+	sort_list=[]
+	for z in x:
+		sort_var=z.get('first_name')
+		sort_list.append(sort_var)
+	y=y+1
+	count=Counter(sort_list).most_common(1)
+	print("Самое распространенное имя среди учеников класса {}: {}".format(y,count[0][0]))
+
+
+task="Задание 4"
 # Для каждого класса нужно вывести количество девочек и мальчиков в нём.
+print(task)
 school = [
 	{'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
 	{'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
@@ -70,15 +90,32 @@ is_male = {
 	'Олег': True,
 	'Миша': True,
 }
-# ???
 
 # Пример вывода:
 # В классе 2a 2 девочки и 0 мальчика.
 # В классе 3c 0 девочки и 2 мальчика.
 
+count_gender=[]
+for x in school:
+	female=0
+	male=0
+	class_num=x.get('class')
+	class_students=x.get('students')
+	for y in class_students:
+		name=y.get('first_name')
+		for z in is_male:
+			if name==z:
+				if is_male.get(z)==True:
+					male=male+1
+				else:
+					female=female+1
+	count_gender.append()
+	print("В классе {} учатся {} мальчиков и {} девочек.".format(class_num, male, female))
 
-# Задание 5
+
+task="Задание 5"
 # По информации о учениках разных классов нужно найти класс, в котором больше всего девочек и больше всего мальчиков.
+print(task)
 school = [
 	{'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
 	{'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
@@ -89,8 +126,23 @@ is_male = {
 	'Олег': True,
 	'Миша': True,
 }
-# ???
 
 # Пример вывода:
 # Больше всего мальчиков в классе 3c
 # Больше всего девочек в классе 2a
+
+for x in school:
+	female=0
+	male=0
+	class_num=x.get('class')
+	class_students=x.get('students')
+	for y in class_students:
+		name=y.get('first_name')
+		for z in is_male:
+			if name==z:
+				if is_male.get(z)==True:
+					male=male+1
+				else:
+					female=female+1
+	print("В классе {} учатся {} больше всего мальчиков и {} девочек.".format(class_num, male, female))
+
